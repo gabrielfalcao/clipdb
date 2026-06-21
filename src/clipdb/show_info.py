853743ee65f9c277836d2d39aef83afd0cf01233
Gabriel Falcão <gabrielteratos@gmail.com>
2026-06-21 21:15:43 +0000

    fixes typo

--- src/clipdb/show_info.py
+++ src/clipdb/show_info.py
@@ -2,9 +2,10 @@ import os
 import re
 import sys
 
-
-TERM_REGEX = re.compile(r'^(?P<type>xterm)(([-](?P<color_variant>256color|truecolor))?)$', re.I)
-COLORTERM_REGEX = re.compile(r'^(?P<type>truecolor)$', re.I)
+TERM_REGEX = re.compile(
+    r"^(?P<type>xterm)(([-](?P<color_variant>256color|truecolor))?)$", re.I
+)
+COLORTERM_REGEX = re.compile(r"^(?P<type>truecolor)$", re.I)
 
 
 up = "\x1b[Am"
@@ -24,27 +25,29 @@ reset_up_and_rewind = f"{reset}{up}{rewind}"
 
 
 def show_info_truecolor(message):
-    sys.stderr.write(f'{bg_truecolor}{fg_truecolor}{message}{reset}')
+    sys.stderr.write(f"{bg_truecolor}{fg_truecolor}{message}{reset}")
+
 
 def show_info_256_color(message):
-    sys.stderr.write(f'{bg_256color}{fg_256color}{message}{reset}')
+    sys.stderr.write(f"{bg_256color}{fg_256color}{message}{reset}")
+
 
 def show_info_base_color(message):
-    sys.stderr.write(f'{bg_base_color}{fg_base_color}{message}{reset}')
+    sys.stderr.write(f"{bg_base_color}{fg_base_color}{message}{reset}")
 
 
 def show_info(*args, **kw):
-    term_var = (os.getenv('TERM') or '').lower()
-    colorterm_var = (os.getenv('COLORTERM') or '').lower()
+    term_var = (os.getenv("TERM") or "").lower()
+    colorterm_var = (os.getenv("COLORTERM") or "").lower()
     term = TERM_REGEX.search(term_var)
     colorterm = COLORTERM_REGEX.search(colorterm_var)
 
     sys.stderr.write(f"{reset_up_and_rewind}")
     sys.stderr.flush()
 
-    if 'truecolor' in colorterm_var
+    if "truecolor" in colorterm_var:
         show_info_truecolor(*args, **kw)
-    elif '256color'  in term_var:
+    elif "256color" in term_var:
         show_info_256_color(*args, **kw)
     else:
         show_info_basecolors(*args, **kw)
