084aa696e7ffb9995f86de9280ac4954dc69ba64
Gabriel Falcão <gabrielteratos@gmail.com>
2026-06-21 21:29:30 +0000

    skip current frame

--- src/clipdb/__init__.py
+++ src/clipdb/__init__.py
@@ -1,10 +1,14 @@
 import ipdb
-
+import inspect
 
 from clipdb.show_info import show_info
 
 
 def set_trace(*args, **kw) -> None:
+    if not 'frame' in kw.keys():
+        curframe = inspect.currentframe()
+        kw['frame'] = curframe.f_back
+
     try:
         ipdb.set_trace(*args, **kw)
     except KeyboardInterrupt:
